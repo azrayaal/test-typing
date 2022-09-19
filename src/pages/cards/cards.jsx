97fa3c9d1ae4a0dab2c10957c3fbc6f4e3254150
AzraYazid <azrayazidalkautsar@gmail.com>
2022-09-19 18:20:16 +0700

    update function

--- src/pages/cards/cards.jsx
+++ src/pages/cards/cards.jsx
@@ -5,22 +5,56 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import './cards.css';
+import { useRef, useState } from 'react';
+
+// const getCloud = () => {
+//   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid at molestias atque quod, fuga consequatur aliquam aspernatur, odio repellat asperiores adipisci eligendi inventore expedita dignissimos laborum beatae commodi dicta, laboriosam rem voluptate. Tempora, dolor voluptatem quis sunt tempore, illum beatae, sit numquam aspernatur labore rem laudantium blanditiis aliquid quae culpa.'
+//     .split(' ')
+//     .sort(() => (Math.random() > 0.5 ? 1 : -1));
+// };
+const getCloud = () =>
+  `loreLorem ipsum dolor sit amet consectetur adipisicing elit. Magnam temporibus, sequi fugiat dolore tempora cumque ducimus nihil excepturi nisi quasi atque libero. Hic pariatur incidunt laudantium sunt ipsam, alias harum tempora dignissimos reprehenderit, doloribus optio magnam repellat, officiis beatae facere?m40`.split(
+    ' '
+  );
 
 export default function KolomType() {
+  const [userInput, setUserInput] = useState('');
+  const [activeWordIndex, setActiveWordIndex] = useState(0);
+  const cloud = useRef(getCloud());
+
+  // function Word(props) {
+  //   return <span>{props.text} </span>;
+  // }
+
+  function processInput(value) {
+    if (value.endsWith(' ')) {
+      // the value has finished this word
+      setActiveWordIndex((index) => index + 1);
+      setUserInput('');
+    } else {
+      setUserInput(value);
+    }
+  }
   return (
     <Card body className=" rounded " id="outer">
       <Card body style={{ backgroundColor: '#E4F4F3' }}>
-        <div className="fs-4" style={{ textAlign: 'justify' }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid at molestias atque quod, fuga consequatur aliquam aspernatur, odio repellat asperiores adipisci eligendi inventore expedita dignissimos laborum beatae commodi dicta,
-          laboriosam rem voluptate. Tempora, dolor voluptatem quis sunt tempore, illum beatae, sit numquam aspernatur labore rem laudantium blanditiis aliquid quae culpa.
-        </div>
+        <p className="fs-4" style={{ textAlign: 'justify' }}>
+          {cloud.current.map((word, index) => {
+            if (index === activeWordIndex) {
+              return <b>{word} </b>;
+            }
+            return <span>{word} </span>;
+          })}
+        </p>
       </Card>
       <br />
       <br />
       <Card body style={{ backgroundColor: '#E4F4F3' }}>
         <InputGroup size="lg">
           {/* <InputGroup.Text id="inputGroup-sizing-lg">Large</InputGroup.Text> */}
-          <Form.Control aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+          <Form.Control aria-label="Large" aria-describedby="inputGroup-sizing-sm" type="text" value={userInput} onChange={(e) => processInput(e.target.value)} />
+
+          {/* <input type="text" value={userInput} onChange={(e) => processInput(e.target.value)} /> */}
         </InputGroup>
         <Row>
           <Col md={6} className="mt-3">
