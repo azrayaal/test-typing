699d3a30d9e4d07fc11d45e4fb0e7e646015c1e2
AzraYazid <azrayazidalkautsar@gmail.com>
2022-09-20 11:15:28 +0700

    update Timer

--- src/pages/cards/cards.jsx
+++ src/pages/cards/cards.jsx
@@ -5,45 +5,104 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import './cards.css';
-import { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
-// const getCloud = () => {
-//   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid at molestias atque quod, fuga consequatur aliquam aspernatur, odio repellat asperiores adipisci eligendi inventore expedita dignissimos laborum beatae commodi dicta, laboriosam rem voluptate. Tempora, dolor voluptatem quis sunt tempore, illum beatae, sit numquam aspernatur labore rem laudantium blanditiis aliquid quae culpa.'
-//     .split(' ')
-//     .sort(() => (Math.random() > 0.5 ? 1 : -1));
-// };
 const getCloud = () =>
   `loreLorem ipsum dolor sit amet consectetur adipisicing elit. Magnam temporibus, sequi fugiat dolore tempora cumque ducimus nihil excepturi nisi quasi atque libero. Hic pariatur incidunt laudantium sunt ipsam, alias harum tempora dignissimos reprehenderit, doloribus optio magnam repellat, officiis beatae facere?m40`.split(
     ' '
   );
 
+function Word(props) {
+  const { text, active, correct } = props;
+
+  if (correct === true) {
+    return <span className="correct">{text} </span>;
+  }
+  if (correct === false) {
+    return <span className="incorrect">{text} </span>;
+  }
+  if (active) {
+    return <span className="active">{text} </span>;
+  }
+  return <span>{text} </span>;
+}
+
+function Timer(props) {
+  const { startCounting, correctWords } = props;
+  const [timeElapsed, setTimeElapsed] = useState(0);
+
+  useEffect(() => {
+    let id;
+    if (startCounting) {
+      id = setInterval(() => {
+        // do something
+        setTimeElapsed((oldTime) => oldTime + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(id);
+    };
+  }, [startCounting]);
+
+  const minutes = timeElapsed / 60;
+
+  return (
+    <div>
+      <p>Time: {timeElapsed}</p>
+      <p>speed: {(correctWords / minutes || 0).toFixed(2)} WPM</p>
+    </div>
+  );
+}
+
+Word = React.memo(Word);
+
 export default function KolomType() {
   const [userInput, setUserInput] = useState('');
   const [activeWordIndex, setActiveWordIndex] = useState(0);
+  const [correctWordArray, setCorrectWordArray] = useState([]);
+  const [startCounting, setStartCounting] = useState(false);
   const cloud = useRef(getCloud());
 
-  // function Word(props) {
-  //   return <span>{props.text} </span>;
-  // }
-
   function processInput(value) {
+    if (activeWordIndex === cloud.current.length) {
+      // stop
+      return;
+    }
+
+    if (!startCounting) {
+      setStartCounting(true);
+    }
+
     if (value.endsWith(' ')) {
       // the value has finished this word
+
+      if (activeWordIndex === cloud.current.length - 1) {
+        // overflow
+        setStartCounting(false);
+        setUserInput('Completed');
+      } else {
+        setUserInput('');
+      }
+
       setActiveWordIndex((index) => index + 1);
-      setUserInput('');
+      // correct word
+      setCorrectWordArray((data) => {
+        const word = value.trim();
+        const newResult = [...data];
+        newResult[activeWordIndex] = word === cloud.current[activeWordIndex];
+        return newResult;
+      });
     } else {
       setUserInput(value);
     }
   }
+
   return (
     <Card body className=" rounded " id="outer">
       <Card body style={{ backgroundColor: '#E4F4F3' }}>
         <p className="fs-4" style={{ textAlign: 'justify' }}>
           {cloud.current.map((word, index) => {
-            if (index === activeWordIndex) {
-              return <b>{word} </b>;
-            }
-            return <span>{word} </span>;
+            return <Word text={word} active={index === activeWordIndex} correct={correctWordArray[index]} />;
           })}
         </p>
       </Card>
@@ -52,7 +111,7 @@ export default function KolomType() {
       <Card body style={{ backgroundColor: '#E4F4F3' }}>
         <InputGroup size="lg">
           {/* <InputGroup.Text id="inputGroup-sizing-lg">Large</InputGroup.Text> */}
-          <Form.Control aria-label="Large" aria-describedby="inputGroup-sizing-sm" type="text" value={userInput} onChange={(e) => processInput(e.target.value)} />
+          <Form.Control aria-label="Large" aria-describedby="inputGroup-sizing-sm" type="text" value={userInput} onChange={(e) => processInput(e.target.value)} placeholder="Start Typing" />
 
           {/* <input type="text" value={userInput} onChange={(e) => processInput(e.target.value)} /> */}
         </InputGroup>
@@ -64,6 +123,7 @@ export default function KolomType() {
             <Button className="mt-3 float-end d-flex align-items-center" variant="danger">
               RESET
             </Button>
+            <Timer startCounting={startCounting} correctWords={correctWordArray.filter(Boolean).length} />
           </Col>
         </Row>
       </Card>
